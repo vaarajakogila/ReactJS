08f8ea1665a3d63eced378ba1c4cee7bbdb68eb1
vaaraja kogila <vaarajak@gmail.com>
2025-02-21 20:57:49 +0530

    CSS loading error fixed

--- pages/start.js
+++ pages/start.js
@@ -1,15 +1,17 @@
-import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import styles from "../styles/Start.module.css";
 
 export default function Start() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <Container>
-      <h1>Get Started</h1>
-      <p>Begin your journey with us.</p>
-    </Container>
+    <div className={mounted ? styles.startPage : ""}>
+      <h1>Start Page</h1>
+      <p>Welcome to the start page.</p>
+    </div>
   );
 }
-
-const Container = styled.div`
-  padding: 20px;
-  text-align: center;
-`;
